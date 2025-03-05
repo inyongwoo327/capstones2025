187d2ee00904310caf4b1758165638181e8bc5a7
Evan Woo <inyongwoo327@gmail.com>
2025-03-05 13:56:49 +0100

    Re-added frontend as a normal directory

---
+++ automateAWSRepo/frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted } from "vue";
+
+export default defineComponent({
+  setup() {
+    const message = ref("Loading...");
+
+    onMounted(async () => {
+      try {
+        const response = await fetch("http://localhost:3000/");
+        const data = await response.json();
+        message.value = data.message;
+      } catch (error) {
+        message.value = "Error fetching data";
+      }
+    });
+
+    return { message };
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>

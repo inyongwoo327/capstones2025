24719d318d2741461eb77539af36a87ffc92d4d3
Evan Woo <inyongwoo327@gmail.com>
2025-03-15 21:21:03 +0100

    Added permissions

--- automateAWSRepo/terraform/s3.tf
+++ automateAWSRepo/terraform/s3.tf
@@ -9,3 +9,26 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
     ignore_public_acls      = true
     restrict_public_buckets = true
 }
+
+resource "random_id" "frontend_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-app-${terraform.workspace}-${random_id.frontend_suffix.hex}"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "random_id" "lambda_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "lambda-deployment-${terraform.workspace}-${random_id.lambda_suffix.hex}"
+}
--- automateAWSRepo/terraform/s3_lambda.tf
+++
@@ -1,7 +0,0 @@
-resource "random_id" "lambda_suffix" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "lambda-deployment-${terraform.workspace}-${random_id.lambda_suffix.hex}"
-}

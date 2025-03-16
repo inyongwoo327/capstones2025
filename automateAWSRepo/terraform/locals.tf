fd99d4ebd8a966303aefff11e7fdac76b421eea7
Evan Woo <inyongwoo327@gmail.com>
2025-03-17 00:54:02 +0100

    Workflow terraform version change

--- automateAWSRepo/terraform/locals.tf
+++ automateAWSRepo/terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  environment           = "${terraform.workspace}"
-  s3_bucket_backend     = "lambda-deployment-${terraform.workspace}"
-  s3_bucket_frontend    = "frontend-app-${terraform.workspace}"
-  api_gateway_name      = "backend-api-${terraform.workspace}"
-  lambda_function_name  = "backend-lambda-${terraform.workspace}"
+  environment          = terraform.workspace
+  s3_bucket_backend    = "lambda-deployment-${terraform.workspace}"
+  s3_bucket_frontend   = "frontend-app-${terraform.workspace}"
+  api_gateway_name     = "backend-api-${terraform.workspace}"
+  lambda_function_name = "backend-lambda-${terraform.workspace}"
 }
--- automateAWSRepo/terraform/s3.tf
+++ automateAWSRepo/terraform/s3.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-state-1741561765"
+  bucket = "terraform-state-1741561765"
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
-    bucket = "terraform-state-1741561765"
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  bucket                  = "terraform-state-1741561765"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "random_id" "frontend_suffix" {

5cdbaf2255483f38c9322702eb4e03efc2c9d120
Evan Woo <inyongwoo327@gmail.com>
2025-03-18 00:31:48 +0100

    terraform format conduct

--- automateAWSRepo/terraform/locals.tf
+++ automateAWSRepo/terraform/locals.tf
@@ -1,8 +1,8 @@
 locals {
-  environment          = terraform.workspace
-  s3_bucket_backend    = "${var.s3_bucket_backend}-${terraform.workspace}"
-  s3_bucket_frontend   = "${var.s3_bucket_frontend}-${terraform.workspace}"
-  api_gateway_name     = "${var.api_gateway_name}-${terraform.workspace}"
-  lambda_function_name = "${var.lambda_function_name}-${terraform.workspace}"
+  environment                  = terraform.workspace
+  s3_bucket_backend            = "${var.s3_bucket_backend}-${terraform.workspace}"
+  s3_bucket_frontend           = "${var.s3_bucket_frontend}-${terraform.workspace}"
+  api_gateway_name             = "${var.api_gateway_name}-${terraform.workspace}"
+  lambda_function_name         = "${var.lambda_function_name}-${terraform.workspace}"
   cloudfront_distribution_name = "${var.cloudfront_distribution_name}-${terraform.workspace}"
 }
--- automateAWSRepo/terraform/variables.tf
+++ automateAWSRepo/terraform/variables.tf
@@ -14,35 +14,35 @@ variable "s3_bucket_backend" {
   description = "S3 bucket for storing Lambda deployment package"
   type        = string
   # Remove dynamic default, require input or set via locals
-  default     = "backend-deployment"
+  default = "backend-deployment"
 }
 
 variable "s3_bucket_frontend" {
   description = "S3 bucket for hosting frontend application"
   type        = string
   # Remove dynamic default, require input or set via locals
-  default     = "frontend-deployment"
+  default = "frontend-deployment"
 }
 
 variable "api_gateway_name" {
   description = "API Gateway name"
   type        = string
   # Remove dynamic default, require input or set via locals
-  default     = "backend-api"
+  default = "backend-api"
 }
 
 variable "lambda_function_name" {
   description = "AWS Lambda function name"
   type        = string
   # Remove dynamic default, require input or set via locals
-  default     = "backend-lambda"
+  default = "backend-lambda"
 }
 
 variable "cloudfront_distribution_name" {
   description = "Name of the CloudFront distribution"
   type        = string
   # Remove dynamic default, require input or set via locals
-  default     = "frontend-cdn"
+  default = "frontend-cdn"
 }
 
 variable "lambda_runtime" {
@@ -61,7 +61,7 @@ variable "iam_role" {
   description = "IAM Role for AWS services"
   type        = string
   # Replace with dynamically created role ARN if using Terraform to create the role
-  default     = "arn:aws:iam::590184075527:role/GitHubActionsRole"
+  default = "arn:aws:iam::590184075527:role/GitHubActionsRole"
 }
 
 variable "terraform_state_bucket" {
